0c0d8386c181d0cf52aff2ac213ed3581caab875
Team1557 <team1557@gmail.com>
2019-02-10 17:08:14 -0500

    Revert "Fixed powNoSignChange to not multipy result by 10"
    This reverts commit 4e5af660f23f942f8319b0ae5c526156ef5f5ff9.

--- The_Real_Project_Oxygen/src/main/java/frc/robot/Constants_And_Equations.java
+++ The_Real_Project_Oxygen/src/main/java/frc/robot/Constants_And_Equations.java
@@ -47,9 +47,9 @@ public class Constants_And_Equations {
         double dten = d*10;
         double temp = Math.pow(dten, pow);
         if (checkValuePositive(dten) == checkValuePositive(temp)){
-            return temp/10;
+            return temp;
         } else {
-            return temp/10 * -1;
+            return temp * -1;
         }
     }
 
