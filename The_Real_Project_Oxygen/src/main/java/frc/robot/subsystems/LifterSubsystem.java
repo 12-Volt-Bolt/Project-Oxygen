501c66c23f62e0f6816dec6297e21ac6bfdc5f8e
Team1557 <team1557@gmail.com>
2019-02-22 19:04:18 -0500

    Added checkLiftSaftey Method

--- The_Real_Project_Oxygen/src/main/java/frc/robot/subsystems/LifterSubsystem.java
+++ The_Real_Project_Oxygen/src/main/java/frc/robot/subsystems/LifterSubsystem.java
@@ -7,7 +7,9 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.OI;
 
 /**
  * Add your docs here.
@@ -16,9 +18,34 @@ public class LifterSubsystem extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
+  // global variables
+  public static int mode = 0;
+
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
+  }
+
+  // checks to see if the current lift saftey mode needs updating and returns the new mode
+  // 0 = safety on
+  // 1 = manual mode
+  // 2 = step mode
+  // 3 = reset lifter
+  public static int checkLiftSaftey(){
+
+    if (OI.oneSlotController.getBumper(Hand.kLeft) == true && OI.oneSlotController.getBumper(Hand.kRight) == true) {
+      if (OI.oneSlotController.getX(Hand.kLeft) > 0.9 && OI.oneSlotController.getX(Hand.kRight) < -0.9) {
+        mode = 1;
+      } else if (OI.oneSlotController.getX(Hand.kLeft) < -0.9 && OI.oneSlotController.getX(Hand.kRight) > 0.9) {
+        mode = 2;
+      } else if (OI.oneSlotController.getStickButton(Hand.kLeft) == true && OI.oneSlotController.getStickButton(Hand.kRight) == true) {
+        mode = 3;
+      }
+    } else {
+      mode = 0;
+    }
+
+    return mode;
   }
 }
